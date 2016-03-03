d7d02140f200be1dc0d335f1bad34ce25b57e2a5
stevenscorfield <steven.scorfield@gmail.com>
2016-03-03 09:28:46 +0000

    Added files via upload

---
+++ C/WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreatePerson();
+        }
+
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Person thisPerson = this.DataContext as Person;
+            textBlock.Text = "Your name is " + thisPerson.Name + " and your phone number is " + thisPerson.PhoneNumber;
+        }
+
+        private void CreatePerson()
+        {
+            Person person1 = new Person();
+            person1.Name = "";
+            person1.PhoneNumber = "";
+
+            this.DataContext = person1;
+        }
+    }
+    public class Person
+    {
+        public string Name;
+        public string PhoneNumber;
+    }
+}
+
